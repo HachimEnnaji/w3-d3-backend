5aa7f402136f42c1eb9463601e0ab85e880f0a55
HachimEnnaji <ashennaji@hotmail.it>
2024-02-14 17:31:17 +0100

    Aggiungere i file di progetto.

---
+++ Cinema/Cinema/Default.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.UI;
+
+namespace Cinema
+{
+    public partial class _Default : Page
+    {
+        int CountSalaNord = 0;
+        int CountSalaSud = 0;
+        int CountSalaEst = 0;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString.ToString();
+            SqlConnection database = new SqlConnection(connectionString);
+
+            try
+            {
+                database.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = database;
+                cmd.CommandText = $"SELECT Sala,Ridotto, COUNT(*) AS nPrenotazioni FROM Prenotazioni  GROUP BY Sala,Ridotto ORDER BY Sala ASC";
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string sala = reader["sala"].ToString();
+                    int count = Convert.ToInt32(reader["nPrenotazioni"]);
+
+                    switch (sala)
+                    {
+
+
+                        case "SALA NORD":
+                            CountSalaNord += count;
+                            break;
+                        case "SALA SUD":
+                            CountSalaSud += count;
+
+                            break;
+                        case "SALA EST":
+                            CountSalaEst += count;
+
+
+                            break;
+                    }
+                    bigliettiNord.InnerHtml = CountSalaNord.ToString();
+                    bigliettiRidottiNord.InnerHtml = count.ToString();
+
+                    bigliettiSud.InnerHtml = CountSalaSud.ToString();
+                    bigliettiRidottiSud.InnerHtml = count.ToString();
+
+
+                    bigliettiEst.InnerHtml = CountSalaEst.ToString();
+                    bigliettiRidottiEst.InnerHtml = count.ToString();
+
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Errore");
+                Response.Write(ex.Message);
+            }
+            finally { database.Close(); }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString.ToString();
+            SqlConnection database = new SqlConnection(connectionString);
+
+
+            try
+            {
+                database.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = database;
+                cmd.CommandText = $"INSERT INTO Prenotazioni (Nome , Cognome , Sala, Ridotto) VALUES (@Nome, @Cognome, @NomeSala,@Ridotto)";
+
+                cmd.Parameters.AddWithValue("@Nome", nome.Text);
+                cmd.Parameters.AddWithValue("@Cognome", cognome.Text);
+                cmd.Parameters.AddWithValue("@NomeSala", DropDownList1.SelectedValue);
+
+                if (ridotto.Checked)
+                {
+                    cmd.Parameters.AddWithValue("@Ridotto", true);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@Ridotto", false);
+                }
+                cmd.ExecuteNonQuery();
+
+                demo.InnerHtml = "Acquisto andato a buon fine!";
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Errore");
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                database.Close();
+                nome.Text = string.Empty;
+                cognome.Text = string.Empty;
+                ridotto.Checked = false;
+            }
+
+        }
+    }
+}
---
+++ Cinema/Cinema/Default.aspx.designer.cs
@@ -0,0 +1,170 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Questo codice è stato generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace Cinema
+{
+
+
+    public partial class _Default
+    {
+
+        /// <summary>
+        /// Controllo demo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl demo;
+
+        /// <summary>
+        /// Controllo nome.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nome;
+
+        /// <summary>
+        /// Controllo rfvName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvName;
+
+        /// <summary>
+        /// Controllo cognome.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox cognome;
+
+        /// <summary>
+        /// Controllo RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
+        /// Controllo DropDownList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
+        /// Controllo ridotto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox ridotto;
+
+        /// <summary>
+        /// Controllo Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Controllo Salanord.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell Salanord;
+
+        /// <summary>
+        /// Controllo bigliettiNord.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell bigliettiNord;
+
+        /// <summary>
+        /// Controllo bigliettiRidottiNord.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell bigliettiRidottiNord;
+
+        /// <summary>
+        /// Controllo Salasud.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell Salasud;
+
+        /// <summary>
+        /// Controllo bigliettiSud.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell bigliettiSud;
+
+        /// <summary>
+        /// Controllo bigliettiRidottiSud.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell bigliettiRidottiSud;
+
+        /// <summary>
+        /// Controllo SalaEst.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell SalaEst;
+
+        /// <summary>
+        /// Controllo bigliettiEst.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell bigliettiEst;
+
+        /// <summary>
+        /// Controllo bigliettiRidottiEst.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell bigliettiRidottiEst;
+    }
+}
